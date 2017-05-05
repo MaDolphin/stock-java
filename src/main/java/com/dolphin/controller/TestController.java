6cedeb4c97e1c86c3d9015f37eebc9c15d8dd5fa
Haikun Huang <haikun.huang@sap.com>
2017-05-05 18:03:59 +0800

    hhk

--- src/main/java/com/dolphin/controller/TestController.java
+++ src/main/java/com/dolphin/controller/TestController.java
@@ -5,6 +5,8 @@ import com.dolphin.service.StockService;
 import com.dolphin.util.HttpClientUtil;
 import org.apache.http.HttpEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +16,8 @@ import javax.annotation.Resource;
 /**
  * Created by huang on 2017/5/1.
  */
-@RestController
+@RequestMapping("/main")
+@Controller
 public class TestController {
 
     @Resource
@@ -22,6 +25,12 @@ public class TestController {
 
     @Autowired
     private Sender sender;
+
+    @RequestMapping("/index")
+    public String Index(Model model) {
+//        model.addAttribute("name", "Dolphin");
+        return "index";
+    }
 
     @RequestMapping("/")
     public String index() {
